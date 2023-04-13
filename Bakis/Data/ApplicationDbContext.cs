0c28d6e37dbdbb7a711087d149f59a4ee3b26af4
AurimasKubilius <aurimas.kubilius@voicehit.com>
2023-04-13 22:13:29 +0300

    Added connection string to appsettings.json

--- Bakis/Data/ApplicationDbContext.cs
+++ Bakis/Data/ApplicationDbContext.cs
@@ -18,12 +18,16 @@ namespace Bakis.Data
         public DbSet<UserRoom> UserRooms { get; set; }
         public DbSet<UserCompatibility> UserCompatibilities { get; set; }
 
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-Bakis-53bc9b9d-9d6a-45d4-8429-2a2761773502;");
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -37,6 +41,5 @@ namespace Bakis.Data
 
             // Other configurations ...
         }
-
     }
 }
